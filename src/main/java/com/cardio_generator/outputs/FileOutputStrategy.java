0dba8d2e50a44abc205bf33c64a7e784909f7426
i6377212 <k.grzeskiewicz@student.maastrichtuniversity.nl>
2025-04-04 12:48:50 +0200

    Task 3: Style Guide Errors

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -6,14 +6,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
-
-public class fileOutputStrategy implements OutputStrategy {
+//Changed class first letter to match file name
+public class FileOutputStrategy implements OutputStrategy {
 
     private String BaseDirectory;
 
     public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
-
-    public fileOutputStrategy(String baseDirectory) {
+    //Changed constructor name to match class name
+    public FileOutputStrategy(String baseDirectory) {
 
         this.BaseDirectory = baseDirectory;
     }
@@ -28,14 +28,16 @@ public class fileOutputStrategy implements OutputStrategy {
             return;
         }
         // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+
+        //Changed variable name to lowerCase
+        String filePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+                Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
-            System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
         }
     }
 }
